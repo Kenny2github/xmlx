2db09b9dc72fd0b2a2d63df17f9ac4431674fbfb
Kenny2github <kenny2minecraft@gmail.com>
2017-08-30 19:54:28 +0800

    Fix real-world situations ._. and general bugfixes; added documentation.

--- xmlx.py
+++ xmlx.py
@@ -7,7 +7,10 @@ class Element(): #element class, here we go!
     def __hash__(self):
         '''Hash of the element.
 
-The return value is the hash of a combination of the element's name, text, and attributes, so the hashes of two elements with the same name, text, and attributes will compare equal.'''
+The return value is the hash of a combination of
+the element's name, text, and attributes, so the
+hashes of two elements with the same name, text,
+and attributes will compare equal.'''
         return hash(tuple(self.attrib.items()) + (self.name, self.text))
     def __init__(self, text): #INIT
         '''Initialize the element from a string.
@@ -31,18 +34,18 @@ self.children - a list of sub-elements in this element.
 
 self.childrendict - a dictionary of sub-elements in the
 format "name":<element>. {"b":<b>...</b>} for this case'''
-        text = re.sub(r'<(?:!|\?).*?>', '', text, flags=re.S) #remove all SGML weird stuff and comments
-        if re.match(r'(?!<(.*?)(?: .*?)*?>.*?</\1>)(<.*?(?: .*?)*?/>)', text, re.S): #if the element is <foo/> and not <foo>bar</foo>
-            self.name = re.match('<(.*?)(?: .*?)*?/>', text, re.S).group(1) #get the name
+        text = re.sub(r'<[!\?].*?>', '', text, flags=re.S).strip() #remove all SGML weird stuff and comments and make sure we can match it
+        if re.match(r'(?!<(.*?).*?>.*?</\1>)(<.*?/>)', text, re.S): #if the element is <foo/> and not <foo>bar</foo>
+            self.name = re.match('<(.*?)(?:(?= .*?).*?)??/>', text, re.S).group(1) #get the name
             self.attrib = {} #no attributes yet
-            for m in re.finditer(' (?P<name>.*?)="(?P<value>.*?)"', re.search('<.*?((?: .*?)*?) ?/>', text, re.S).group(1), flags=re.S): #for every attrib="value"
+            for m in re.finditer(' (?P<name>.*?)=[\'"](?P<value>.*?)[\'"]', re.search('<.*?((?: .*?)*?) ?/>', text, re.S).group(1), flags=re.S): #for every attrib="value"
                 self.attrib[m.group('name')] = m.group('value') #attrib=value
             self.content = '' #since this is <foo/> there's no content
             self.text = text #outerHTML
             self.children = [] #or children
             self.childrendict = {} #^^
         else: #it's <foo>bar</foo> after all
-            self.name = re.match(r'<(.*?)(?: .*?)*?>.*?</\1>', text, re.S).group(1) #get the name
+            self.name = re.match(r'<(.*?).*?>.*?</\1>', text, re.S).group(1) #get the name
             self.attrib = {} #no attributes yet
             for m in re.finditer(' (?P<name>.*?)="(?P<value>.*?)"', re.search('<.*?((?: .*?)*?)>', text, flags=re.S).group(1), flags=re.S): #for every attrib="value"
                 self.attrib[m.group('name')] = m.group('value') #attrib=value
@@ -59,19 +62,35 @@ format "name":<element>. {"b":<b>...</b>} for this case'''
                     self.childrendict[c.name].append(c) #add this element to list of elements with its name
 
 def elemdict(text):
-    root = Element(text)
-    elem = {'?': root.attrib}
-    for c in root.children:
-        if not c.name in elem:
-            elem[c.name] = [elemdict(c.text)]
-        else:
-            elem[c.name].append(elemdict(c.text))
-    elem['@'] = root.content
-    elem['*'] = root.text
+    """Make a dictionary of elements instead of an object.
+
+In an element dict, the key @ is its innerHTML, * is
+its outerHTML, and ? is its attributes as a dict.
+
+Any other keys are lists of child elements of that name."""
+    root = Element(text) #just quickly make an element out of it anyway
+    elem = {'?': root.attrib} #store its attributes under the key ?
+    for c in root.children: #for every child
+        if not c.name in elem: #if the child's name hasn't been recorded yet
+            elem[c.name] = [elemdict(c.text)] #recursively add its name and itself
+        else: #otherwise
+            elem[c.name].append(elemdict(c.text)) #just add the element to its list under its name
+    #note: there's no point in having its children as a plain list.
+    elem['@'] = root.content #innerHTML
+    elem['*'] = root.text #outerHTML
     return elem
 
 def rchildren(elem,indent=0): #recursively print children
-    result = '|-' * indent + str(elem) #add itself to the string
+    """Recursively print an element's children.
+
+The return value is a string in the format:
+base element
+|-child element
+| |-grandchild element
+| |-another grandchild element
+| | |-great-grandchild element
+|-another child element"""
+    result = ('| ' * (indent - 1)) + ('|-' * bool(indent)) + str(elem) #add itself to the string
     for child in elem.children: #and for every one of its children
         result += '\n' + rchildren(child, indent+1) #add it with one more indent
     return result #finally return the string
@@ -87,5 +106,6 @@ if __name__ == '__main__': #demo html
     </body>
 </html>''' #demo file
     test = Element(DOM) #make an element out of the DOM
-    print(rchildren(test)) #recursively print its children
-    print(elemdict(DOM)) #print the dictionary form
+    print('Original DOM:\n' + DOM)
+    print('\nRecursively printed objectified DOM:\n' + rchildren(test)) #recursively print its children
+    print('\nDictified DOM:\n' + str(elemdict(DOM))) #print the dictionary form
