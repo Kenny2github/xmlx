a8d90d72d3e46b7111ceefb1114f1e2822233e41
Kenny2github <kenny2minecraft@gmail.com>
2017-08-30 12:16:12 +0800

    Version 0.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name='xmlx',
-    version='0.0.0',
+    version='0.0.1',
     description='A simple and compact XML parser.',
     long_description=ldesc,
     url='https://github.com/Kenny2github/xmlx',
